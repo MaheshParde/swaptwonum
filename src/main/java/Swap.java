3041b6d2a726c1f91356058f7853c4ca4be23117
LAPTOP-58L05II1\parde <pardemahesh9595@gmai.com>
2021-02-04 19:32:21 +0530

    forth commit

--- src/main/java/Swap.java
+++ src/main/java/Swap.java
@@ -1,8 +1,8 @@
 public class Swap {
     public static void main(String[] args) {
 
-        int a = 10, b = 20,c,d;
-
+        int a = 10, b = 20,c,d,e;
+        System.out.println("hello user!!")
         System.out.println("--Before swap--");
         System.out.println("First number = " + a);
         System.out.println("Second number = " + b);
